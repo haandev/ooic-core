773b107d6cf6c7a6177b906b92deb54be88f1cec
haandev <62476477+haandev@users.noreply.github.com>
2022-03-08 23:45:56 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -34,10 +34,12 @@ export async function ooic(config: OoicConfig) {
   await connect();
   await initRouter(app);
   try {
-    await import("./../../../src/model/model_relation_map.ts)
-  } catch (err) {
-    console.warn("Missing model relation mapper. Please put 'model_relation_map.ts' in src/model directory.")
-  } 
+    if (fs.existsSync("src/model/model_relation_map.ts")) {
+      await import("./../../../src/model/model_relation_map")
+    } else {
+      console.warn("Missing model relation mapper. Please put 'model_relation_map.ts' in src/model directory.")
+    }
+  } catch (err) {}
   await initErrorHandlers(app);
   app.use(unhandled);
 
